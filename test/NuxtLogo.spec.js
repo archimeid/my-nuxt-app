e9e2d481d7fbf735d9c4912c72ccaa06ba48b3bb
Archimeid <68751170+archimeid@users.noreply.github.com>
2022-04-09 01:35:26 +0200

    Change NuxtLogo test

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,9 +1,18 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import NuxtLogo from '@/components/NuxtLogo.vue'
 
-describe('NuxtLogo', () => {
-  test('is a Vue instance', () => {
+const factory = () => {
+  return shallowMount(NuxtLogo, {});
+}
+
+describe("NuxtLogo", () => {
+  test("is a Vue instance", () => {
     const wrapper = mount(NuxtLogo)
     expect(wrapper.vm).toBeTruthy()
+  })
+
+  test("renders properly", () => {
+    const wrapper = factory()
+    expect(wrapper.html()).toMatchSnapshot()
   })
 })
